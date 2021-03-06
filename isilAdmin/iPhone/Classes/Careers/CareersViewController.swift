78f800cfd0e388cc6a19fa48e33a54ef252f7618
Milumon <milumon@outlook.com>
2021-03-05 17:54:32 -0800

    show venues

--- isilAdmin/iPhone/Classes/Careers/CareersViewController.swift
+++ isilAdmin/iPhone/Classes/Careers/CareersViewController.swift
@@ -6,75 +6,20 @@
 //
 
 import UIKit
-import Foundation
-import Firebase
 
-class CareersViewController: UIViewController, UITableViewDelegate
-
-{
-    
-    @IBOutlet weak var tblCareer: UITableView!
-    var careersList = [CareerBE]()
-    var obj: CareerBE!
+class CareersViewController: UIViewController {
 
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        self.tblCareer.delegate = self
-        self.tblCareer.dataSource = self
-        self.loadData()
-        
+
+       
     }
     
-    func loadData(){
-        
-    self.database.child("Carreras").child("Ingenieria").observe(DataEventType.value, with: {(snapshot) in
-            print("snapshot", snapshot)
-            if snapshot.childrenCount > 0 {
-                self.careersList.removeAll()
-
-                for career in snapshot.children.allObjects as! [DataSnapshot] {
-                    
-                     
-                    let careerObject = career.value as? [String: AnyObject]
-                    
-                    let name  = careerObject?["nombre"] as! String
-                    let description  = careerObject?["descripcion"] as! String
-                    let career = CareerBE(name: name, description: description)
-                    
-                    
-                    self.careersList.append(career)
-                }
-
-                DispatchQueue.main.async {
-                    self.tblCareer.reloadData()
-                }
-                //self.tableCourses.reloadData()
-            }
-            
-        })
-   }
-
+    @IBAction func showListCareers(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "ListCareersViewController", sender: self)
+    }
+    
+    @IBAction func showFormCareers(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "FormCareersViewController", sender: self)
+    }
 }
-
-extension CareersViewController: UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.careersList.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        print("no te quiere")
-          
-        let cellIdentifier = "celCareer"
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! CareerTableViewCell
-        cell.obj = self.careersList[indexPath.row]
-        
-        return cell
-        
-    }
-    
-    
-}
-
--- isilAdmin/iPhone/Classes/Locations/LocationsViewController.swift
+++ isilAdmin/iPhone/Classes/Locations/LocationsViewController.swift
@@ -1,9 +1,4 @@
-//
-//  LocationsViewController.swift
-//  isilAdmin
-//
-//  Created by kerwin Joel on 2/23/21.
-//
+
 
 import UIKit
 
@@ -12,18 +7,16 @@ class LocationsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func showListVenues(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "ListVenuesViewController", sender: self)
     }
-    */
+    
+    
+    @IBAction func showFromVenues(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "FormVenuesViewController", sender: self)
+    }
+    
 
 }
